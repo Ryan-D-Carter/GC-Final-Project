68116de3822547a0c0f9b15e42ad5ca4b8a549f3
Ryan <RyanCarterMI@gmail.com>
2021-09-09 21:55:36 -0400

    Ryan Commit

---
+++ GC_Final_Project/Controllers/MetController.cs
@@ -0,0 +1,27 @@
+﻿using GC_Final_Project.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GC_Final_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MetController : ControllerBase
+    {
+        private readonly MetObjectDAL _metObjectDAL = new MetObjectDAL();
+
+        [HttpGet("getObjById/{id}")]
+        public async Task<ActionResult<int>> IndexAsync(int id)
+        {
+            var metObj = await _metObjectDAL.GetMetObjectById(id);
+
+            return metObj;
+        }
+
+
+    }
+}
---
+++ GC_Final_Project/Models/MetObjectDAL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using static GC_Final_Project.Models.MetObject;
+
+namespace GC_Final_Project.Models
+{
+    public class MetObjectDAL
+    {
+        private HttpClient GetHttpClient()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://collectionapi.metmuseum.org");
+            return client;
+        }
+
+        //getObjById/{id}
+        public async Task<int> GetMetObjectById(int id)
+        {
+            var client = GetHttpClient();
+
+            var response = await client.GetAsync($"/public/collection/v1/objects/{id}");
+
+            var metObj = await response.Content.ReadAsAsync<MetObject.Rootobject>();
+
+            return metObj.objectID;
+        }
+
+
+    }
+}
