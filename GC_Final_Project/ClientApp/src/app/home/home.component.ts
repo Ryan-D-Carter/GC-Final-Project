9b334b2aa44f804d199862a8daa5022c461ccec2
okshelleyo <oconorshe012@gmail.com>
2021-09-16 21:21:17 -0400

    class 9/16

--- GC_Final_Project/ClientApp/src/app/home/home.component.ts
+++ GC_Final_Project/ClientApp/src/app/home/home.component.ts
@@ -1,7 +1,9 @@
-import { ListBySearchTerm } from '../models/ListBySearchTerm';
+import { ListBySearchTerm } from './../models/ListBySearchTerm';
 import { Component } from '@angular/core';
 import { StringifyOptions } from 'querystring';
 import { MetAPIService } from '../services/met-api.service';
+import { MetObjects } from '../models/MetObjects';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -17,15 +19,25 @@ export class HomeComponent {
   5. onClickLike methods & onClickDislike methods?
   */
 
+  mediumArray: string[] = [
+    "painting", "ceramic","sculpture", "printing",  "costume", "drawings",
+    "photograph", "textile", "furniture", "jewelery"
+  ];
 
   listBySearchTerm: ListBySearchTerm[] =[];
-  dummySearchTerm: string = "painting"
+  dummySearchTerm: string = "painting";
+  searchTerm: string = '';
+  metObj: MetObjects[] =[];
+  //selection: ListBySearchTerm = {total: 0, objectIDs: []};
 
-  constructor(private metAPIservice: MetAPIService) { }
+  constructor(private metAPIservice: MetAPIService) {
+    this.getMetObjById(838076);
+  }
 
 
   ngOnInit() {
     //this.getObjListBySearchTerm(this.dummySearchTerm);
+
    }
 
   getObjListBySearchTerm(searchTerm: string) {
@@ -38,7 +50,22 @@ export class HomeComponent {
     );
   }
 
-  // get
+
+  getMetObjById(objectId: number) {
+    this.metAPIservice.getObjectById(objectId).subscribe(
+      result => {
+        this.metObj = result;
+        console.log(this.metObj);
+      },
+      error => console.log(error)
+    );
+  }
+
+  //when the user presses select, it should send the value to get objects by search term
+  submitSearchTerm(form: NgForm) {
+    this.searchTerm = form.form.value;
+    console.log(this.searchTerm);
+  }
 
 
 }
