f1cebaf5f52597ca4213e47a62caf6ba34b29d95
okshelleyo <oconorshe012@gmail.com>
2021-09-18 12:47:03 -0400

    updated comments

--- GC_Final_Project/ClientApp/src/app/met-app/met-app.component.ts
+++ GC_Final_Project/ClientApp/src/app/met-app/met-app.component.ts
@@ -19,19 +19,18 @@ export class MetAppComponent implements OnInit {
        "photograph", "textile", "furniture", "jewelery"
      ];
   randomValue: number;
-  //testArray = [1 , 2, 3, 4 , 5, 6, 7, 8, 9, 10]
 
 
   constructor(private metAPIservice: MetAPIService) { }
 
-
-  ngOnInit() {
+    // On start, the app shows this pre-selected object. Can work out a better system later.
+    ngOnInit() {
       console.log("made it to the app component")
       this.getMetObjById(436529);
    }
 
-   //when the user selects a searchterm from the drop down, this method assigns the value to property userSearchSelection
-    //then sets that value as a parameter in the function to find the list of ojects from the search term (below)
+    //when the user selects a searchterm from the drop down, this method assigns the value to property userSearchSelection
+    //then, that value is sent as a parameter to getObjListBySearchTerm, which assigns values to a property called listBySearchTerm
     submitSearchTerm(e: any) {
       this.userSearchSelection = e.target.value;
         console.log(this.userSearchSelection);
@@ -40,7 +39,7 @@ export class MetAppComponent implements OnInit {
 
     }
 
-    //accesses the API to return a list of object IDs (number array) (model: listBySearchTerm)
+    //accesses the 3rd party API to assign the values the listBySearchTerm object
     getObjListBySearchTerm(searchTerm: string) {
         console.log("beginning of getObjListBySearchterm")
         console.log(`term: ${searchTerm}`);
@@ -53,17 +52,19 @@ export class MetAppComponent implements OnInit {
          error => console.log(error)
       )};
 
-   //then we need to get a random number from that list of Object IDs
+   //this method returns a random number from a number array. It gets a random index from the array
+   //then assigns the value at that index to property randomValue.
+   //improvement needed: make sure the random number doesn't repeat
    getRandomValue(array: number[]) :number {
       console.log("beginning of getObjListBySearchterm getRandomValue")
       var randIndex = Math.floor(Math.random() * array.length)
        console.log(randIndex);
-       this.randomValue = array[randIndex];
+      this.randomValue = array[randIndex];
        console.log(this.randomValue);
-       return this.randomValue;
-     }
+      return this.randomValue;
+    }
 
-   //and send that random number to this method, which will get the JSON of object details, which we will display.
+   //accesses the 3rd party API to assign values to the MetObject object
    getMetObjById(objectId: number) {
      this.metAPIservice.getObjectById(objectId).subscribe(
        result => {
@@ -71,18 +72,25 @@ export class MetAppComponent implements OnInit {
          console.log(this.metObj);
        },
        error => console.log(error)
-   )};
+      )};
 
+   //Refers to the Select button, which is clicked after a search term is selected.
+   //This sets a local variable rand equal to the random value that is returned from getRandomValue
+   //then sends rand to getMetObjById to display the metObject
     onSelect() {
       var rand = this.getRandomValue(this.listBySearchTerm.objectIDs);
       this.getMetObjById(rand);
     }
 
+    //same as onSelect, but when the user selects "like"
+    //***Required functionality: add the metObject to the MyGallery using the TheLike add function****
     onLike() {
       var rand = this.getRandomValue(this.listBySearchTerm.objectIDs);
       this.getMetObjById(rand);
     }
 
+    //same as onSelect, but when the user selects "dislike"
+    //nothing else happens per MVP goals
     onDislike() {
       var testvalue = this.getRandomValue(this.listBySearchTerm.objectIDs);
       this.getMetObjById(testvalue);
